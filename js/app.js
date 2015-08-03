5b06204d9a3d385a6ae1ebf03ec6fa04150bf355
Jonathan Schapiro <jschapir@gmail.com>
2015-08-03 11:49:28 -0700

    datastructures populated

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 
-//create roomate models
+//initializations
 var roomateNames = ['Jon','Payam','Steven','Adam','Prad'];
 var roomates = new Roomates({model:Roomate});
+var tasks = new Tasks({model:Task});
+
+var loadDishWasher = new Task({
+	taskName:'Load the DishWasher',
+	pointValue:10,
+	dailyDouble:false
+});
+
+var emptyDishWasher = new Task({
+	taskName:'Empty the DishWasher',
+	pointValue:20,
+	dailyDouble:false
+});
+
+var takeOutTrash = new Task({
+	taskName:'Take Out the Trash',
+	pointValue:25,
+	dailyDouble:true
+});
+
+var prize = new Prize({
+	name:'Chipotle',
+	dollarValue:9.00
+});
+
+//add models to respective collections
+
 _.each(roomateNames,function(name){
 	//create model and add to collection
 	roomates.add(new Roomate({
@@ -9,7 +36,14 @@ _.each(roomateNames,function(name){
 	}));
 });
 
-var tasks = new Tasks({model:Task});
+tasks.add(loadDishWasher);
+tasks.add(emptyDishWasher);
+tasks.add(takeOutTrash);
+
+
+
+
+
 
 
 

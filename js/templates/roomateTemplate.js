1e52eda1d7a0b85d990372ec4443ff180a87542b
Jonathan Schapiro <jschapir@gmail.com>
2015-08-03 12:27:32 -0700

    working on views and templates

---
+++ js/templates/roomateTemplate.js
@@ -0,0 +1,15 @@
+<div class = 'roomate'>
+	<div class = 'roomateName'> <%= name %> </div>
+	<div class ='roomateImage'> 
+		<img src ='<%= image %>'></img>
+	</div>
+
+	<div class ='roomateScoreContainer'>
+
+		<p> Points: <span> <%= points %> </span> </p>
+		<p> Most Recent: <span> <%= mostRecentTask %> </span> </p>
+
+	</div>
+
+
+</div>
---
+++ js/views/roomateView.js
@@ -0,0 +1,11 @@
+var RoomateView = Backbone.View.extend({
+
+	el: '',
+	initialize: function(){
+
+		this.render();
+	},
+	render:function(){
+
+	}
+});
